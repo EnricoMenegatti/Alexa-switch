2f567b53f44256ef8ecc2e71d4466a0803c40276
Enrico Menegatti <enricomenegatti13@gmail.com>
2020-04-15 11:37:10 +0200

    rimosse sezioni esp32. sistemata tabulazione e impaginazione.

--- Alexa-switch/Alexa-switch.ino
+++ Alexa-switch/Alexa-switch.ino
@@ -1,34 +1,83 @@
 /*
  * This is an example on how to use Espalexa alongside an ESP8266WebServer.
  */ 
+#define ESPALEXA_MAXDEVICES 1
+
 #include "Espalexa.h"
-#ifdef ARDUINO_ARCH_ESP32
-  #include <WiFi.h>
-  #include <WebServer.h> //if you get an error here please update to ESP32 arduino core 1.0.0
-#else
-  #include <ESP8266WiFi.h>
-  #include <ESP8266WebServer.h>
-#endif
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-// prototypes
-boolean connectWifi();
-
-//callback functions
-void firstLightChanged(uint8_t brightness);
+bool wifiConnected = false;
 
 // Change this!!
 const char* ssid = "Vodafone-Menegatti_plus";
 const char* password = "Menegatti13";
 
-boolean wifiConnected = false;
-
 Espalexa espalexa;
-#ifdef ARDUINO_ARCH_ESP32
-  WebServer server(80);
-#else
-  ESP8266WebServer server(80);
-#endif
+ESP8266WebServer server(80);
 
+//FUNCTIONS----------------------------------------------------------------------------------------------------------------
+void alphaChanged(EspalexaDevice* d) 
+{
+  if (d == nullptr) return; //this is good practice, but not required
+  
+  Serial.print("A changed to ");
+  if (d -> getValue())
+  {
+    Serial.println("ON");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(D1, HIGH);
+  }
+  else 
+  {
+    Serial.println("OFF");
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(D1, LOW);
+  }
+}
+
+
+// connect to wifi – returns true if successful or false if not
+boolean connectWifi()
+{
+  boolean state = true;
+  int i = 0;
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+    if (i > 20)
+    {
+      state = false; 
+      break;
+    }
+    i++;
+  }
+  Serial.println("");
+  if (state)
+  {
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else 
+  {
+    Serial.println("Connection failed.");
+  }
+  delay(100);
+  return state;
+}
+
+//SETUP--------------------------------------------------------------------------------------------------------------------
 void setup()
 {
   Serial.begin(115200);
@@ -51,11 +100,12 @@ void setup()
     });
 
     // Define your devices here.
-    espalexa.addDevice("Prova", firstLightChanged); //simplest definition, default state off
+    espalexa.addDevice("Prova", alphaChanged, EspalexaDeviceType::onoff); //non-dimmable device
 
     espalexa.begin(&server); //give espalexa a pointer to your server object so it can use your server instead of creating its own
     //server.begin(); //omit this since it will be done by espalexa.begin(&server)
-  } else
+  } 
+  else
   {
     while (1)
     {
@@ -65,70 +115,13 @@ void setup()
   }
 
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(16, OUTPUT);
   pinMode(D1, OUTPUT);
 }
  
+//MAIN---------------------------------------------------------------------------------------------------------------------
 void loop()
 {
    //server.handleClient() //you can omit this line from your code since it will be called in espalexa.loop()
    espalexa.loop();
    delay(1);
-}
-
-//our callback functions
-void firstLightChanged(uint8_t brightness) {
-    Serial.print("Device 1 changed to ");
-    
-    //do what you need to do here
-
-    //EXAMPLE
-    if (brightness == 255) {
-      Serial.println("ON");
-      digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(D1, HIGH);
-    }
-    else if (brightness == 0) {
-      Serial.println("OFF");
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(D1, LOW);
-    }
-    else {
-      Serial.print("DIM "); Serial.println(brightness);
-      //analogWrite(LED_BUILTIN, brightness);
-    }
-}
-
-// connect to wifi – returns true if successful or false if not
-boolean connectWifi(){
-  boolean state = true;
-  int i = 0;
-  
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-  Serial.println("Connecting to WiFi");
-
-  // Wait for connection
-  Serial.print("Connecting...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    if (i > 20){
-      state = false; break;
-    }
-    i++;
-  }
-  Serial.println("");
-  if (state){
-    Serial.print("Connected to ");
-    Serial.println(ssid);
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-  }
-  else {
-    Serial.println("Connection failed.");
-  }
-  delay(100);
-  return state;
 }

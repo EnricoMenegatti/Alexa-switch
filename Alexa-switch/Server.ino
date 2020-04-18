4a3f655b94e868c533179a1f7925ae2b24cd5ddc
Enrico Menegatti <enricomenegatti13@gmail.com>
2020-04-18 23:02:57 +0200

    creato metodo di ricezione parametri da web server.

--- Alexa-switch/Server.ino
+++ Alexa-switch/Server.ino
@@ -2,6 +2,10 @@
 void Start_Server() // Start a HTTP server with a file read handler and an upload handler
 {
   server.on("/", handleRoot);
+  
+  //server.on("/PostParam.html", handlePlain);
+  server.on("/Submit", HTTP_POST, handlePlain);
+  
   server.on("/Edit.html", HTTP_POST, []() 
   {
     server.send(200, "text/plain", "");
@@ -31,25 +35,30 @@ void handleRoot()
         <head>\
             <title>ESP-Alexa switch</title>\
             <link href='main.css' rel='stylesheet' type='text/css'>\
-            <script src='main.js' type='text/javascript'></script>\
         </head>\
+        \
         <body>\
             <center>\
                 <H1>Configurazione ESP-Alexa switch</H1>\
-                <form class='form-1'>\
+                \
+                <form class='form-1' method='post' action='/Submit'>\
                     <H2>Wi-Fi</H2>\
                     <label for='ssid'>SSID</label>\
                     <input type='text' id='ssid' name='ssid' maxlength='40' value=%s>\
                     <br><br>\
+                    \
                     <label for='pw'>Password</label>\
                     <input type='password' id='pw' name='pw' maxlength='40' value=%s>\
                     <br><br><br>\
+                    \
                     <H2>Dispositivo</H2>\
                     <label for='name'>Nome</label>\
                     <input type='text' id='name' name='name' maxlength='40' value=%s>\
-                    <input type='reset' value='Reimposta'>\
-                    <input type='button' value='Salva' onclick='Save_Eeprom()'>\
+                    \
+                    <input class='button' type='reset' value='Reimposta'>\
+                    <input class='button' type='submit' value='Salva'>\
                 </form>\
+                \
                 <div>\
                   <p id='debug'></p>\
                 </div>\
@@ -57,4 +66,25 @@ void handleRoot()
         </body>\
     </html>", ssid, password, Device_Name);
     server.send(200, "text/html", root);
+}
+
+void handlePlain() 
+{
+  Serial.println("-------------");
+  String s = server.arg("plain");
+  Serial.println(s);
+
+  server.arg("ssid").toCharArray(ssid, 40);
+  server.arg("pw").toCharArray(password, 40);
+  server.arg("name").toCharArray(Device_Name, 40);
+
+  if(fileWiFiConfig('write') && fileDeviceConfig('write'))
+  {
+    server.sendHeader("Location", "/"); // Redirect the client to the success page
+    server.send(303); 
+  } 
+  else 
+  {
+    server.send(500, "text/plain", "500: couldn't create file");
+  }
 }

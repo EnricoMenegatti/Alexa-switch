59016f874fb6755aa915dc6286ce9aea517df2ec
Enrico Menegatti <enricomenegatti13@gmail.com>
2020-04-20 00:14:41 +0200

    sposyayi codici html su file separato.

--- Alexa-switch/HTML.ino
+++ Alexa-switch/HTML.ino
@@ -1,36 +1,4 @@
 
-void Start_Server() // Start a HTTP server with a file read handler and an upload handler
-{
-  server.on("/", handleRoot);
-  server.on("/Info", handleRoot);
-  
-  server.on("/SubmitWiFi", HTTP_POST, handleSubWiFi);
-  server.on("/SubmitDevice", HTTP_POST, handleSubDevice);
-
-  server.on("/WiFi", handleWiFi);
-  server.on("/Device", handleDevice);
-  
-  server.on("/Edit.html", HTTP_POST, []() 
-  {
-    server.send(200, "text/plain", "");
-  }, handleFileUpload); // go to 'handleFileUpload'
-  
-  server.onNotFound(handleNotFound); // if someone requests any other file or page, go to function 'handleNotFound'
-                                     // and check if the file exists
-}
-
-void handleNotFound() // if the requested file or page doesn't exist, return a 404 not found error
-{
-  if(!handleFileRead(server.uri())) // check if the file exists in the flash memory (SPIFFS), if so, send it
-  {
-    if (!espalexa.handleAlexaApiCall(server.uri(),server.arg(0))) //if you don't know the URI, ask espalexa whether it is an Alexa control request
-    {
-      //whatever you want to do with 404s
-      server.send(404, "text/plain", "Not found");
-    }
-  }
-}
-
 void handleRoot() 
 {
   char root[2048];
@@ -97,26 +65,6 @@ void handleWiFi()
     server.send(200, "text/html", wifi);
 }
 
-void handleSubWiFi() 
-{
-  Serial.println("WiFi Config Submit");
-  String s = server.arg("plain");
-  Serial.println(s);
-
-  server.arg("ssid").toCharArray(ssid, 40);
-  server.arg("pw").toCharArray(password, 40);
-
-  if(fileWiFiConfig('write'))
-  {
-    server.sendHeader("Location", "/WiFi"); // Redirect the client to the success page
-    server.send(303); 
-  } 
-  else 
-  {
-    server.send(500, "text/plain", "500: couldn't create file");
-  }
-}
-
 void handleDevice() 
 {
   char device[2048];
@@ -152,21 +100,59 @@ void handleDevice()
     server.send(200, "text/html", device);
 }
 
-void handleSubDevice() 
+void handleAdvanced() 
 {
-  Serial.println("Device Config Submit");
-  String s = server.arg("plain");
-  Serial.println(s);
-  
-  server.arg("name").toCharArray(Device_Name, 40);
-
-  if(fileDeviceConfig('write'))
-  {
-    server.sendHeader("Location", "/Device"); // Redirect the client to the success page
-    server.send(303); 
-  } 
-  else 
-  {
-    server.send(500, "text/plain", "500: couldn't create file");
-  }
+  char advanced[2048];
+  snprintf(advanced, 2048,
+    "<html>\
+        <head>\
+            <title>ESP-Alexa switch</title>\
+            <link href='main.css' rel='stylesheet' type='text/css'>\
+            <script src='main.js' type='text/javascript'></script>\
+        </head>\
+        \
+        <body onload='Startup()'>\
+            <center>\
+                <H1>Configurazione ESP-Alexa switch</H1>\
+                \
+                <ul class='sidenav'>\
+                    <li><a class='content' href='/Info'>Info</a></li>\
+                    <li><a class='content' href='/WiFi'>WiFi</a></li>\
+                    <li><a class='content' href='/Device'>Dispositivo</a></li>\
+                    <li><a class='active' href='/Advanced'>Avanzate</a></li>\
+                </ul>\
+                \
+                <div class='form-1'>\
+                    <H2>Avanzate</H2>\
+                    <table>\
+                        <tr>\
+                            <td class='first' rowspan='2'><label>Reset</label></td>\
+                            <td class='second'><p class='adv'>Abilita</p></td>\
+                            <td class='third' rowspan='2'>\
+                                <form method='post' action='/SubmitReset'>\
+                                    <input class='button' id='reset' type='submit' value='Reset'>\
+                                </form>\
+                            </td>\
+                        </tr>\
+                        <tr>\
+                            <td class='second'><input type='checkbox' id='checkReset' onclick='Reset()'></td>\
+                        </tr>\
+                        <tr>\
+                            <td class='first' rowspan='2'><label>Ripristino dati di Fabbrica</label></td>\
+                            <td class='second'><p class='adv'>Abilita</p></td>\
+                            <td class='third' rowspan='2'>\
+                                <form method='post' action='/SubmitRipr'>\
+                                    <input class='button' id='ripristino' type='submit' value='Ripristino'>\
+                                </form>\
+                            </td>\
+                        </tr>\
+                        <tr>\
+                            <td class='second'><input type='checkbox' id='checkRipr' onclick='Ripristino()'></td>\
+                        </tr>\
+                    </table>\
+                </div>\
+            </center>\
+        </body>\
+    </html>");
+    server.send(200, "text/html", advanced);
 }
